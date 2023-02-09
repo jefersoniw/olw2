3310cf922b235d7fd3ba8a679459fa1fc4b9d9cf
Jeferson Chagas <jeferson_chagas25@hotmail.com>
2023-02-08 21:36:03 -0300

    Criação de migrations para criação de tabelas na database.

--- database/migrations/2023_02_09_003514_create_user_email_index.php
+++ database/migrations/2023_02_09_003514_create_user_email_index.php
@@ -13,15 +13,9 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('user_email_index', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email');
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
             $table->timestamps();
-            $table->softDeletes();
         });
     }
 
@@ -32,6 +26,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('user_email_index');
     }
 };
