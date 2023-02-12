1a1453e6eb1ebb00f734bf3ce0c2d509cbb85a9f
Jeferson Chagas <jeferson_chagas25@hotmail.com>
2023-02-11 22:52:55 -0300

    Criação de seeders e população das tabelas do banco de dados com as factorys e seeders.

--- database/factories/SellerFactory.php
+++ database/factories/SellerFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -18,7 +19,7 @@ class SellerFactory extends Factory
     {
         return [
             'company_id' => fake()->numberBetween(1, 4),
-            'user_id' => User::factory()
+            'user_id' => User::factory(),
         ];
     }
 }
